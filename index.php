2cd888455a1f0afe50019879e26f5a9c719563f4
bakarisanja <milos.babic@mi-system.co.rs>
2015-09-04 14:32:39 +0200

    comit with ssh

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * sdf Step 1: Require the Slim Framework
- *sdfsdfsdfsdfsdfsdfsdfsdfsdf
+ *milos ssh
  * If you are not using Composer, you need to require the
  * Slim Framework and register its PSR-0 autoloader.
  *
